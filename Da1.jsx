1e5caafe4e91a9ee38862e54cc913307771dd094
AmzadT <amzadhussain7250@gmail.com>
2024-01-05 00:35:40 +0530

    Changes

---
+++ Da1.jsx
@@ -0,0 +1,94 @@
+// React -> Js Framework, -> Fast
+// HTML + CSS + JS < ReactJs
+
+// Html -> .html <body></body>
+// css -> .css <style></style>
+// js -> .js <script></script>
+
+// React -> css -> style.module.css || style.css
+//       -> .jsx -> index.jsx
+//       -> jsx -> Javascript Xml -> html + CSS + Javascript code
+
+// ** Advantages/benefits of using ReactJs -> 
+
+//    1. Fast loading code,
+//    2. Javascript Framework,
+//    3. We can write html, css, and js code in a single line in a single file,
+//    4. We can use different css libraries to make our UI look good, ex; Chakra UI, Material UI, Talwind css, Bootstrap.,
+
+//    5. 
+//    index.js -> Parent code.
+//    App.js   -> Parent code.
+
+//    * Commands 
+//    1. Create a react app -> npx create-react-app app-name
+//    2. cd root directory -> head over to the root directory
+//    3.  npm start -> to run your react app.
+
+
+//    * You want to make 20 pages:
+//    1. Create Different Components folder
+//    2. Create your files inside the Components
+
+//    * .jsx file structure
+//    1. import React from 'react'
+//    2. Make Function
+//    3. Export
+//    4. import function in App and use it
+
+//    * useState hook
+//    1. hook function that used to store the data and update the data;
+//    2. you'll have to import useState from react,
+//    3. syntax -> 
+//    const [name, setName] = useState("Amzad");
+//    console.log(name) -> Amzad;
+
+//    * useEffect ->
+//    1. Run all the code only once
+
+
+
+
+
+
+
+//  Introduction of React
+
+//  React is a library that allows you to create a web application that is fast, efficient, and user-friendly. It is a JavaScript library that is used to build user interfaces. It is a library that allows you to create a web application that is fast, efficient, and user-friendly. It is a library that allows you to.
+
+//  React Owner --> FaceBook
+
+//  1. React is a JavaScript library for building user interfaces.
+
+// 2. React is used to build single-page applications.
+
+// 3. React allows us to create reusable UI Components.
+
+
+
+//  What is React ?
+
+// 1. React is a declarative, efficient and flexible JavaScript library for building user interfaces. it lets you compose complex UIs from small and isolated pieces of code called "Components".
+
+// 2. React, sometimes referred to as a Frontend javascript Framework, is a JavaScript library created by FaceBook.
+
+// 3. React is a tool for build UI components.
+
+
+
+//  How does React Work
+
+// Instead of manipulating the DOM directly, React uses virtual DOM to create a virtual representation of the DOM. This virtual DOM is then compared to the real DOM to determine if any changes need to be made. This comparison is done using a virtual DOM diff algorithm.
+
+
+// React.JS was first released in 2006. React.JS was first used in 2011 for FaceBook's Newsfeed feature.
+
+//  create-react-app includes built tools such as webpack, Babel and ESLint.
+
+//  FaceBook software engineer, "jordan walke", created React.Js.
+
+
+//  Declarative Approach :-  You tell the browser exactly what to do, instead of telling it what you need. the React declarative Approach abstracts that for us.
+
+// with declarative code, we tell JavaScript what we want to be done, and let JavaScript take care of performing the steps.
+
---
+++ Day2.jsx
@@ -0,0 +1,54 @@
+//  JavaScript was invented/created by "Brendan Eich" in 1995 in Netscape.
+
+//  (ECMA) :-  stands for "European Computer Manufacturers Association".
+
+//  JavaScript --> ECMA International --> ECMAScript
+
+//  HTML --> "W3C" --> World Wide Web Consortium
+
+//  ECMAScript is a subset of JavaScript.
+
+//  Babel :- Babel is a JavaScript compiler
+
+//  Babel :-  It is mainly used to convert ECMAScript 2015+ code into a backwards compatible version of JavaScript in current and older browser or enviroments.
+
+//  Babel can convert JSX syntax
+
+
+
+
+//  What is the need of NPM in node.JS ?
+
+// NPM maneges all the packages and modules for Node.JS and consists of command line client NPM. it gets instelled into the system with installation of Node.JS the required packages and modules.
+
+//  (NPM) :- Node Package Manager.
+
+
+// node -v
+// npm -v
+// npm init
+// cd "File Name"
+// npm install -g create-react-app
+// create-react-app --version
+// create-react-app "App-Name"
+// Npm start (It serves our react app on 3000 port number of localhost).
+
+
+
+
+
+//  index.html --> Render / Display HTML
+//  index.js --> Entry Point
+//  App.js --> Component
+
+
+// React Render HTML :-  React Render HTML to the web page by using a function called ReactDOM.render().
+
+
+// The Render Function :-  The purpose of the function is to display the specified HTML code inside the spesified HTML element.
+
+// But Render Where ?
+
+// There is another folder in the root directory of your React project, named "public". in this folder, there is an index.html file.
+
+
