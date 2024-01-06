704956e9d6082179910db0f1bb80f0c5330e5a9a
AmzadT <amzadhussain7250@gmail.com>
2024-01-06 22:58:34 +0530

    Changes

---
+++ Extra-React/Intro-To-React.jsx
@@ -0,0 +1,49 @@
+// React -> Js Framework, -> Fast
+// HTML + CSS + JS < ReactJs
+
+// Html -> .html <body></body>
+// css -> .css <style></style>
+// js -> .js <script></script>
+
+// React -> css -> style.module.css || style.css
+//       -> .jsx -> index.jsx
+//       -> jsx -> Javascript Xml -> html + CSS + Javascript code
+
+// ** Advantages/benefits of using ReactJs -> 
+
+//    1. Fast loading code,
+//    2. Javascript Framework,
+//    3. We can write html, css, and js code in a single line in a single file,
+//    4. We can use different css libraries to make our UI look good, ex; Chakra UI, Material UI, Talwind css, Bootstrap.,
+
+//    5. 
+//    index.js -> Parent code.
+//    App.js   -> Parent code.
+
+//    * Commands 
+//    1. Create a react app -> npx create-react-app app-name
+//    2. cd root directory -> head over to the root directory
+//    3.  npm start -> to run your react app.
+
+
+//    * You want to make 20 pages:
+//    1. Create Different Components folder
+//    2. Create your files inside the Components
+
+//    * .jsx file structure
+//    1. import React from 'react'
+//    2. Make Function
+//    3. Export
+//    4. import function in App and use it
+
+//    * useState hook
+//    1. hook function that used to store the data and update the data;
+//    2. you'll have to import useState from react,
+//    3. syntax -> 
+//    const [name, setName] = useState("Amzad");
+//    console.log(name) -> Amzad;
+
+//    * useEffect ->
+//    1. Run all the code only once
+
+
