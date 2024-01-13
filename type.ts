cb33335161a8eb59d4e4a4e5fea5f7912e154ab9
AmzadT <amzadhussain7250@gmail.com>
2024-01-13 22:38:54 +0530

    Changes

---
+++ type.ts
@@ -0,0 +1,1 @@
+//  TypeScript is a superset of JavaScript that adds static typing and other features to enhance JavaScript development. it was developed by Microsoft on Oct 1, 2012, and is now an open-source project with a large community of contributors
